295727e1ae2657f090dc04897203c2482c9ca345
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2021-09-26 14:05:57 +0530

    feat - quiz functionality

---
+++ scripts/quiz.js
@@ -0,0 +1,27 @@
+var quizForm = document.querySelector("form");
+var checkBtn = document.querySelector(".check-btn");
+var output = document.querySelector(".output")
+
+var answers = {
+    Q1:"90°",
+    Q2:"right angled",
+    Q3:"2",
+    Q4:"trigon",
+    Q5:"trigonometry",
+    Q6:"acute",
+    Q7:"isosceles triangle"
+}
+
+function checkAnswers(){
+    var userAnswers = new FormData(quizForm);
+    var score = 0;
+    for([Q , A] of userAnswers.entries()){
+        if(answers.Q = A){
+            score++;
+        }
+    }
+    output.innerText = `The Score is ${score}`;
+    output.style.display = "block";
+}
+
+checkBtn.addEventListener("click", checkAnswers)
