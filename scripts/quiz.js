de2e7d1b4bd82f6dbf6a998298dc6a3c87f23b4c
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2021-09-27 01:47:42 +0530

    code refactor

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -3,32 +3,32 @@ var checkBtn = document.querySelector(".check-btn");
 var output = document.querySelector(".output")
 
 var answers = {
-    Q1:"90°",
-    Q2:"right angled",
-    Q3:"2",
-    Q4:"trigon",
-    Q5:"trigonometry",
-    Q6:"acute",
-    Q7:"isosceles triangle"
+    Q1: "90°",
+    Q2: "right angled",
+    Q3: "2",
+    Q4: "trigon",
+    Q5: "trigonometry",
+    Q6: "acute",
+    Q7: "isosceles triangle"
 }
 
-function checkAnswers(){
+function checkAnswers() {
     var userAnswers = new FormData(quizForm);
     var score = 0;
     var answerCount = 0;
-    
-    for([Q , A] of userAnswers.entries()){
+
+    for ([Q, A] of userAnswers.entries()) {
         answerCount++;
-        if(answers[Q] === A){
+        if (answers[Q] === A) {
             score++;
         }
     }
-    if(answerCount === 0){
+    if (answerCount === 0) {
         output.innerText = "Attempt atleast one Questions";
         output.style.display = "block";
-    } else{
-    output.innerText = `The Score is ${score}/7`;
-    output.style.display = "block";
+    } else {
+        output.innerText = `The Score is ${score}/7`;
+        output.style.display = "block";
     }
 }
 
